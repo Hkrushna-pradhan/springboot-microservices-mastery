d9fad2b7dc43658e11f55f3918ed86f8d415bd76
Hkrushna-pradhan <hkrushnapradhan@gmail.com>
2025-10-05 06:29:35 +0530

    Updating a file to test commit functionality.

--- BootIOCproj05-MiniProj-RealTimeDI/src/main/java/com/hk/model/Employee.java
+++ BootIOCproj05-MiniProj-RealTimeDI/src/main/java/com/hk/model/Employee.java
@@ -1,12 +1,21 @@
 package com.hk.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Employee {
+	
 	private Integer eno;
+	@NonNull
 	private String ename;
+	@NonNull
 	private String desg;
+	@NonNull
 	private Double salary;
 	private Double grossSalary;
 	private Double netSalary;
